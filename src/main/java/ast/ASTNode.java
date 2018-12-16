4ded22221b703c3e76b46a19a930844b7ab07902
Thales Parreira <tv.parreira@campus.fct.unl.pt>
2018-12-16 18:32:03 +0000

    Modificações aos packages.

--- src/main/java/ast/ASTNode.java
+++ src/main/java/ast/ASTNode.java
@@ -1,10 +1,10 @@
 package ast;
 
+import compiler.Code;
+import compiler.CompilerEnvironment;
 import types.IType;
 import utils.Environment;
 import values.IValue;
-import compiler.Code;
-import compiler.CompilerEnvironment;
 
 public interface ASTNode {
     IValue eval(Environment<IValue> environment) throws Exception;
